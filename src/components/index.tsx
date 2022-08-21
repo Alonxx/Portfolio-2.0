a6078cb06cc167f64220e77db0727c750703dbb3
Alonso Diaz <alonsodiazlip@gmail.com>
2022-08-21 01:11:15 -0300

    fix slider

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,4 +7,3 @@ export { Skills } from "./Skills";
 export { Contact } from "./Contact";
 export { Projects } from "./Projects";
 export { NavBar } from "./NavBar";
-export { Slider } from "./Slider";
